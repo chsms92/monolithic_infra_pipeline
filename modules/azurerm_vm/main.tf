b0a29d5dec76422679ada58a93930416d5e0940b
unknown <chsms92>
2025-11-15 09:12:24 +0530

    script added in vm

--- modules/azurerm_vm/main.tf
+++ modules/azurerm_vm/main.tf
@@ -1,17 +1,7 @@
-data "azurerm_subnet" "subnet" {
-  for_each = var.vms
-  name                 = each.value.subnet_name
-  virtual_network_name = each.value.vnet_name
-  resource_group_name  = each.value.rg_name
-}
-data "azurerm_public_ip" "pip" {
-  for_each = var.vms
-  name                = each.value.pip_name
-  resource_group_name = each.value.rg_name
-}
+
 
 resource "azurerm_network_interface" "nic" {
-    for_each = var.vms
+  for_each            = var.vms
   name                = each.value.nic_name
   location            = each.value.location
   resource_group_name = each.value.rg_name
@@ -25,21 +15,22 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-    for_each = var.vms
-  name                = each.value.vm_name
-  resource_group_name = each.value.rg_name
-  location            = each.value.location
-  size                = each.value.vm_size
-  admin_username      = each.value.admin_username
-  admin_password =     each.value.admin_password
-  disable_password_authentication=false
+  for_each                        = var.vms
+  name                            = each.value.vm_name
+  resource_group_name             = each.value.rg_name
+  location                        = each.value.location
+  size                            = each.value.vm_size
+  admin_username                  = each.value.admin_username
+  admin_password                  = each.value.admin_password
+  custom_data                     = base64decode(file(each.value_script_name))
+  disable_password_authentication = false
   network_interface_ids = [
     azurerm_network_interface.nic[each.key].id,
   ]
 
 
   os_disk {
-    caching              = each.value.os_disk.caching 
+    caching              = each.value.os_disk.caching
     storage_account_type = each.value.os_disk.storage_account_type
   }
 
--- modules/azurerm_vm/variable.tf
+++ modules/azurerm_vm/variable.tf
@@ -1,25 +1,26 @@
 variable "vms" {
-    type=map(object({
-        vm_name = string
-        rg_name = string
-        location = string
-        vm_size = string
-        admin_username = string
-        admin_password = string
-        nic_name = string
-        subnet_name = string
-        vnet_name = string
-        pip_name = string
-        os_disk = object({
-            caching = string
-            storage_account_type = string
-        })
-        source_image_reference = object({
-            publisher = string
-            offer     = string
-            sku       = string
-            version   = string
-        })
-    }))
-  
+  type = map(object({
+    vm_name        = string
+    rg_name        = string
+    location       = string
+    vm_size        = string
+    admin_username = string
+    admin_password = string
+    nic_name       = string
+    subnet_name    = string
+    vnet_name      = string
+    pip_name       = string
+    script_name    = string
+    os_disk = object({
+      caching              = string
+      storage_account_type = string
+    })
+    source_image_reference = object({
+      publisher = string
+      offer     = string
+      sku       = string
+      version   = string
+    })
+  }))
+
 }
